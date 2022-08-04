70bbfd4d9fec0dabd8fd276494eb18ce4a1163d0
xuyang <792865726@qq.com>
2022-08-04 19:26:05 +0800

    chore: update test examples

--- __test__/examples/personnelEvent/multiField.result.vue
+++ __test__/examples/personnelEvent/multiField.result.vue
@@ -9,7 +9,6 @@
     "
     :show-message="false"
   >
-    <!--文本框-->
     <el-input
       :type="
         fieldItem.FTYPE === 'P' || fieldItem.FTYPE === 'N' ? 'number' : 'text'
@@ -61,7 +60,7 @@
         fieldItem.ZZSHM !== 'S'
       "
     ></el-input>
-    <!-- 日期 -->
+
     <el-date-picker
       :class="
         (24 / infoTypeItem.XXCOL) * fieldItem.ZZCOL === 4
@@ -78,7 +77,7 @@
       :name="fieldItem.FIELD"
       v-model="fieldItem.value"
       type="date"
-      placeholder="请选择日期"
+      :placeholder="I18N.$fanyi('请选择日期')"
       value-format="yyyyMMdd"
       @blur="
         fieldItem.ZZDIS !== 'X'
@@ -96,7 +95,7 @@
       "
       v-if="fieldItem.FTYPE === 'D'"
     ></el-date-picker>
-    <!--下拉选择-->
+
     <el-select
       :class="
         (24 / infoTypeItem.XXCOL) * fieldItem.ZZCOL === 4
@@ -109,8 +108,7 @@
           ? 'width97'
           : 'width97-5'
       "
-      filterable
-      v-model="fieldItem.value"
+      filterablev-model="fieldItem.value"
       value-key="KEY"
       @click.native="
         fieldItem.ZZDIS !== 'X'
@@ -149,7 +147,7 @@
         :value="options"
       ></el-option>
     </el-select>
-    <!--多行文本-->
+
     <el-input
       type="textarea"
       rows="5"
@@ -166,11 +164,10 @@
       "
       v-model="fieldItem.value"
       :maxlength="fieldItem.FLENG"
-      show-word-limit
-      :disabled="fieldItem.ZZDIS === 'X'"
+      show-word-limit:disabled="fieldItem.ZZDIS === 'X'"
       v-if="fieldItem.ZKJLX === 'TXT'"
     ></el-input>
-    <!--人员搜索-->
+
     <pa-search-user
       :class="
         (24 / infoTypeItem.XXCOL) * fieldItem.ZZCOL === 4
@@ -187,9 +184,9 @@
       :defaultValue="fieldItem.value ? fieldItem.value.VALUE : ''"
       :ipage="baseParams.MENUII"
       :cheAuth="fieldItem.ZZSHF === 'A' ? 'Z' : ''"
-      @getEmployeeData="dataObject => employeeCallback(dataObject, fieldItem)"
+      @getEmployeeData="(dataObject) => employeeCallback(dataObject, fieldItem)"
       @changed="
-        dataObject =>
+        (dataObject) =>
           selectPaUserCallback(
             dataObject,
             fieldItem,
@@ -221,8 +218,11 @@
           ? dataVerification(group, infoTypeItem, fieldItem)
           : ''
       "
-      :inattrs="{ BANKL: 'KEY', BANKA: 'VALUE' }"
-      placeholder="请选择银行名称"
+      :inattrs="{
+        BANKL: 'KEY',
+        BANKA: 'VALUE'
+      }"
+      :placeholder="I18N.$fanyi('请选择银行名称')"
       v-if="fieldItem.ZZSHM === 'B'"
     ></paSelectBank>
     <template v-if="fieldItem.ZZSHM === 'T'">
@@ -255,7 +255,7 @@
             : ''
         "
         @change="
-          nodeObject =>
+          (nodeObject) =>
             selectNodeCallback(
               nodeObject,
               fieldItem,
@@ -271,7 +271,7 @@
     </template>
 
     <div class="el-form-item__error" v-if="fieldItem.isShowText">
-      非法的{{ fieldItem.FIELN }}
+      {{ I18N.$fanyi('非法的') }} {{ fieldItem.FIELN }}
     </div>
   </el-form-item>
 </template>
@@ -280,7 +280,6 @@
 import paSearchUser from '../components/paSearchUser'
 import paSelectBank from '../components/paSelectBank'
 import paSelectTree from '../components/paSelectTree'
-
 export default {
   name: 'MultiField',
   components: {
@@ -289,13 +288,27 @@ export default {
     paSelectTree
   },
   props: {
-    baseParams: { type: Object },
-    fieldItem: { type: Object },
-    infoTypeItem: { type: Object },
-    group: { type: Object },
-    sonGroup: { type: Object },
-    groupList: { type: Array },
-    verificationList: { type: Array },
+    baseParams: {
+      type: Object
+    },
+    fieldItem: {
+      type: Object
+    },
+    infoTypeItem: {
+      type: Object
+    },
+    group: {
+      type: Object
+    },
+    sonGroup: {
+      type: Object
+    },
+    groupList: {
+      type: Array
+    },
+    verificationList: {
+      type: Array
+    },
     getData: Function
   },
   methods: {
@@ -331,50 +344,84 @@ export default {
         fieldItem.valueList.length > 1
       ) {
         if (fieldItem.emptyFields) {
-          this.emptyField(fieldItem.emptyFields, fieldItem, infoTypeItem) //清空字段的value
+          this.emptyField(fieldItem.emptyFields, fieldItem, infoTypeItem)
+          // 清空字段的value
         }
+
         return
       }
+
       let param = {
         sapInterface: 'PE003',
         IV_PERNR: this.baseParams.employeeNum,
-        IV_INFTY: infty, //信息类型
-        IV_SUBTY: subty, //信息子类型
-        IV_FIELD: field, //字段名称
-        IV_ZACTI: this.baseParams.zaction, //事件编号
-        IV_ACGRP: acgrp, //信息组
+        IV_INFTY: infty,
+
+        // 信息类型
+        IV_SUBTY: subty,
+
+        // 信息子类型
+        IV_FIELD: field,
+
+        // 字段名称
+        IV_ZACTI: this.baseParams.zaction,
+
+        // 事件编号
+        IV_ACGRP: acgrp,
+
+        // 信息组
         IV_USER: '',
-        IV_ACBLK: acblk, //人员信息模板
+        IV_ACBLK: acblk,
+
+        // 人员信息模板
         IV_IPAGE: this.baseParams.MENUII,
         IV_DATA:
           fieldItem.ZZZJL === 'X'
             ? JSON.stringify(this.getDataSearch(fieldItem, infoTypeItem))
-            : '', //是级联数据 否则 为空
+            : '',
+
+        // 是级联数据 否则 为空
         IV_LIKE: like || ''
       }
-      this.$httpServer.sap.baseMethod(param, null, true).then(response => {
+      this.$httpServer.sap.baseMethod(param, null, true).then((response) => {
         let GonsiList = [],
           et_org = [],
           et_list = [],
           et_fields = []
         let ES_MESSAGE = response.ES_MESSAGE
         if (ES_MESSAGE.MSGTY === 'E') return
-        //下拉框的value
+
+        // 下拉框的value
+
         et_list = response.ET_LIST
         this.$set(fieldItem, 'valueList', et_list)
         GonsiList = et_list
         et_org = response.ET_ORG
-        //需要清空字段的值
+
+        // 需要清空字段的值
+
         et_fields = response.ET_FIELDS
-        this.$set(fieldItem, 'emptyFields', et_fields) // 字段里面保存一份需要清空的字段 备用
+        this.$set(fieldItem, 'emptyFields', et_fields)
+        // 字段里面保存一份需要清空的字段 备用
+
         // 需要清空的字段
+
         if (et_fields.length > 0) {
-          this.emptyField(et_fields, fieldItem, infoTypeItem) // 清空字段的value
+          this.emptyField(et_fields, fieldItem, infoTypeItem)
+          // 清空字段的value
         }
+
         // 初始化树结构
-        let treeList = [] // 树结构转换前的数组
-        let treeData = [] // 树结构专用
+
+        let treeList = []
+
+        // 树结构转换前的数组
+
+        let treeData = []
+
+        // 树结构专用
+
         let treeMap = {}
+
         if (et_org.length > 0) {
           for (let i in et_org) {
             let obj = {
@@ -400,23 +447,26 @@ export default {
             obj.leaf = et_org[i].ZLEAF
             treeList.push(obj)
           }
-          treeList.forEach(item => {
+
+          treeList.forEach((item) => {
             treeMap[item.id] = item
           })
-          treeList.forEach(item => {
+          treeList.forEach((item) => {
             let parent = treeMap[item.parent]
+
             if (parent) {
               ;(parent.children || (parent.children = [])).push(item)
             } else {
               treeData.push(item)
             }
           })
-
           this.$set(fieldItem, 'treeData', treeData)
         }
+
         return GonsiList
       })
     },
+
     /**
      * @Copyright  DELOITTE
      * @Description: Change事件，实现类似级联时功能
@@ -434,6 +484,7 @@ export default {
       if (event !== 'X') return
       this.buttonLoading = true
       let defValue = ''
+
       if (fieldItem.ZZSHM === 'T') {
         defValue = JSON.stringify({
           KEY: value.split('-')[0],
@@ -449,6 +500,7 @@ export default {
       } else {
         defValue = value
       }
+
       let param = {
         sapInterface: 'PE009',
         IV_PERNR: this.baseParams.employeeNum,
@@ -465,12 +517,13 @@ export default {
           ZZDEF: defValue
         })
       }
-      this.$httpServer.sap.baseMethod(param).then(response => {
+      this.$httpServer.sap.baseMethod(param).then((response) => {
         this.buttonLoading = false
         let jsonList = '',
           jsonInftyList = '',
           ES_MESSAGE = response.ES_MESSAGE
         if (ES_MESSAGE.MSGTY === 'E') return
+
         if (response.EV_CONFIRM === 'A') {
           this.$confirm(response.EV_TEXT, '提示', {
             confirmButtonText: '确定',
@@ -489,26 +542,37 @@ export default {
             })
             .catch(() => {})
         }
+
         jsonList = response.ET_FIELD
         jsonInftyList = response.ET_INFTY
         let groupList = this.groupList
         let sonGroup
         /**信息类型级联事件 start **/
+
         for (let i in jsonInftyList) {
           for (let j in groupList) {
             if (groupList[j].ACGRP === jsonInftyList[i].ACGRP) {
               sonGroup = groupList[j].ACBLK
+
               for (let k in sonGroup) {
                 if (sonGroup[k].ACBLK === jsonInftyList[i].ACBLK) {
                   let inftyList = sonGroup[k].INFTY
+
                   for (let o in inftyList) {
                     if (
                       inftyList[o].INFTY === jsonInftyList[i].INFTY &&
                       inftyList[o].SUBTY === jsonInftyList[i].SUBTY
                     ) {
-                      inftyList[o].ZZHID = jsonInftyList[i].ZZHID //是否隐藏
-                      inftyList[o].ACTIO = jsonInftyList[i].ACTIO //多条数据的操作类型，控制按钮
-                      inftyList[o].ZZREQ = jsonInftyList[i].ZZREQ //是否必填
+                      inftyList[o].ZZHID = jsonInftyList[i].ZZHID
+
+                      // 是否隐藏
+
+                      inftyList[o].ACTIO = jsonInftyList[i].ACTIO
+
+                      // 多条数据的操作类型，控制按钮
+
+                      inftyList[o].ZZREQ = jsonInftyList[i].ZZREQ
+                      // 是否必填
                     }
                   }
                 }
@@ -519,13 +583,16 @@ export default {
         /**信息类型级联事件 end **/
 
         /**字段级联事件start**/
+
         for (let i in jsonList) {
           for (let j in groupList) {
             if (groupList[j].ACGRP === jsonList[i].ACGRP) {
               sonGroup = groupList[j].ACBLK
+
               for (let k in sonGroup) {
                 if (sonGroup[k].ACBLK === jsonList[i].ACBLK) {
                   let inftyList = sonGroup[k].INFTY
+
                   for (let o in inftyList) {
                     if (
                       inftyList[o].INFTY === jsonList[i].INFTY &&
@@ -535,12 +602,21 @@ export default {
                         inftyList[o].ZZTAB === 'X'
                           ? inftyList[o].models[i]
                           : inftyList[o].FIELD
+
                       for (let n in fieldList) {
                         try {
                           if (fieldList[n].FIELD === jsonList[i].FIELD) {
-                            fieldList[n].ZZDIS = jsonList[i].ZZDIS //是否可输入
-                            fieldList[n].ZZREQ = jsonList[i].ZZREQ //是否必填
-                            fieldList[n].ZZHID = jsonList[i].ZZHID //是否隐藏
+                            fieldList[n].ZZDIS = jsonList[i].ZZDIS
+
+                            // 是否可输入
+
+                            fieldList[n].ZZREQ = jsonList[i].ZZREQ
+
+                            // 是否必填
+
+                            fieldList[n].ZZHID = jsonList[i].ZZHID
+
+                            // 是否隐藏
 
                             if (fieldList[n].ZZSHM === 'T') {
                               if (!jsonList[i].ZZDEF) {
@@ -556,7 +632,10 @@ export default {
                               } else {
                                 let obj = JSON.parse(jsonList[i].ZZDEF)
                                 fieldList[n].value = obj
-                                fieldList[n].valueList = [] //清空原有选项
+                                fieldList[n].valueList = []
+
+                                // 清空原有选项
+
                                 fieldList[n].valueList.push(
                                   JSON.parse(jsonList[i].ZZDEF)
                                 )
@@ -571,7 +650,8 @@ export default {
                               fieldList[n].value = jsonList[i].ZZDEF
                               fieldList[n].ZZBDS = new RegExp(
                                 jsonList[i].ZZBDS.replace(/\s/g, '')
-                              ) //处理正则表达式
+                              )
+                              // 处理正则表达式
                             }
                           }
                         } catch (e) {
@@ -594,6 +674,7 @@ export default {
         /**字段级联事件 end**/
       })
     },
+
     /**
      * @Copyright  DELOITTE
      * @Description: 数据校验
@@ -603,10 +684,11 @@ export default {
      * @param fieldItem 字段
      */
     dataVerification(infoGroupDetail, infoTypeDetail, fieldItem) {
-      //fieldKey 字段key
+      // fieldKey 字段key
       let fieldKey = infoTypeDetail.INFTY + '_' + fieldItem.FIELD
+
       if (fieldItem.ZZBDS !== '' && fieldItem.value !== '') {
-        //值不符合规则
+        // 值不符合规则
         if (!fieldItem.ZZBDS.test(fieldItem.value)) {
           if (!fieldItem.isShowText) {
             this.verificationList.push({
@@ -618,31 +700,37 @@ export default {
               fieldText: fieldItem.FIELN
             })
           }
+
           this.$set(fieldItem, 'isShowText', true)
         } else {
-          //符合规则
+          // 符合规则
           if (fieldItem.isShowText) {
             let dataList = this.verificationList
+
             for (let i in dataList) {
               if (dataList[i]['fieldKey'] === fieldKey) {
                 this.verificationList.splice(i, 1)
               }
             }
           }
+
           this.$set(fieldItem, 'isShowText', false)
         }
       } else {
         if (fieldItem.isShowText) {
           let dataList = this.verificationList
+
           for (let i in dataList) {
             if (dataList[i]['fieldKey'] === fieldKey) {
               this.verificationList.splice(i, 1)
             }
           }
         }
+
         this.$set(fieldItem, 'isShowText', false)
       }
     },
+
     /**
      * @Copyright  DELOITTE
      * @Description: 当前信息类型的value
@@ -654,11 +742,11 @@ export default {
       let iv_data = []
       let obj2 = {}
       let fields = []
-
       let model,
         f,
         df,
         b = false
+
       if (typeItem.ZZTAB === 'X') {
         for (model of typeItem.models) {
           for (f of model) {
@@ -668,6 +756,7 @@ export default {
               break
             }
           }
+
           if (b) {
             break
           }
@@ -689,11 +778,12 @@ export default {
           f.ZZSHM === 'B' ||
           f.ZZSHM === 'S'
         ) {
-          //下拉列表转换格式
+          // 下拉列表转换格式
           obj2[f.FIELD] = (f.value || {}).KEY ? JSON.stringify(f.value) : ''
         } else if (f.ZZSHM === 'T') {
-          //树转换格式
+          // 树转换格式
           let values = f.value.split('-')
+
           if (f.value) {
             obj2[f.FIELD] = JSON.stringify({
               KEY: values[0],
@@ -713,6 +803,7 @@ export default {
       iv_data.push(obj2)
       return iv_data
     },
+
     /**
      * @Copyright  DELOITTE
      * @Description: 清空字段的value
@@ -723,12 +814,12 @@ export default {
      */
     emptyField(emptyfields, field, typeItem) {
       if (emptyfields.length < 1) return
-
       let fields = []
       let model,
         f,
         ef,
         b = false
+
       if (typeItem.ZZTAB === 'X') {
         for (model of typeItem.models) {
           for (f of model) {
@@ -738,6 +829,7 @@ export default {
               break
             }
           }
+
           if (b) {
             break
           }
@@ -749,18 +841,22 @@ export default {
       for (f of fields) {
         for (ef of emptyfields) {
           if (f.FIELD === ef.FIELD) {
-            //如果是树value = ""
+            // 如果是树value = ""
             if (f.ZZSHM === 'T') {
               this.$set(f, 'value', '')
               this.$set(f, 'orgList', [])
             } else {
-              this.$set(f, 'value', { KEY: '', VALUE: '' })
+              this.$set(f, 'value', {
+                KEY: '',
+                VALUE: ''
+              })
               this.$set(f, 'valueList', [])
             }
           }
         }
       }
     },
+
     /**
      * @Copyright  DELOITTE
      * @Description: 树的选择回调
@@ -784,6 +880,7 @@ export default {
         fieldItem
       )
     },
+
     /**
      * @Copyright  DELOITTE
      * @Description: Tree选中的文本
@@ -793,6 +890,7 @@ export default {
     customizedLabel(nodeData) {
       return nodeData.value
     },
+
     /**
      * @Copyright  DELOITTE
      * @Description: 人员回调
@@ -806,6 +904,7 @@ export default {
         VALUE: data.employeeName + '(' + data.employeeNum + ')'
       }
     },
+
     /**
      * @Copyright  DELOITTE
      * @Description: 清空历史选中的人员
@@ -813,8 +912,12 @@ export default {
      * @param fieldItem
      */
     emptEmployeeValue(fieldItem) {
-      fieldItem.value = { KEY: '', VALUE: '' }
+      fieldItem.value = {
+        KEY: '',
+        VALUE: ''
+      }
     },
+
     /**
      * @Copyright  DELOITTE
      * @Description: 选择人员回调
